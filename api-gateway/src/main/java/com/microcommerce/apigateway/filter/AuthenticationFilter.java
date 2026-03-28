71be8c654fdcb12a8cbbad2ba3df91e88a3d5930
Kayinamura-Karimba-Geofrey <geofreykayin@gmail.com>
2026-03-28 21:33:04 +0200

    Implement JWT validation security in API Gateway

---
+++ api-gateway/src/main/java/com/microcommerce/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,49 @@
+package com.microcommerce.apigateway.filter;
+
+import com.microcommerce.apigateway.util.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private RouteValidator validator;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (validator.isSecured.test(exchange.getRequest())) {
+                // Header contains token or not
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("Missing authorization header");
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+                try {
+                    jwtUtils.validateToken(authHeader);
+                } catch (Exception e) {
+                    System.out.println("Invalid Access!");
+                    throw new RuntimeException("Unauthorized access to application");
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+    }
+}
